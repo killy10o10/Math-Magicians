5b6ddb1c8cb154ec449200379a220980b33dd775
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-07-14 02:50:29 +0000

    Fix test for calculate.js

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -2,13 +2,10 @@
 import calculate from '../logic/calculate';
 
 describe('calculate', () => {
-  // test('should return {total: 0, next: null, operation: null} when buttonName is AC', () => {
-  //   expect(calculate({ total: 0, next: null, operation: null }, 'AC')).toEqual({
-  //     total: 0,
-  //     next: null,
-  //     operation: null,
-  //   });
-  // });
+  test('Test AC button on calculator', () => {
+    const result = calculate({ total: '777', next: '75', operation: '' }, 'AC');
+    expect(result).toStrictEqual({ total: null, next: null, operation: null });
+  });
 
   test('should return {total: 7, next: null, operation: null} when buttonName is 3+4', () => {
     expect(calculate({ total: '3', next: '4', operation: '+' }, '=')).toEqual({
